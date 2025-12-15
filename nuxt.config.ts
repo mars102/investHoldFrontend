04d81da39d70e9b750fe4e4b133fd9cd809c2145
mgilyazov <mgilyazov@phoenixit.ru>
2025-12-16 00:18:50 +0300

    Замена иконки

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,7 +29,7 @@ export default defineNuxtConfig({
         { name: 'viewport', content: 'width=device-width, initial-scale=1' }
       ],
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
       ]
     }
   },
